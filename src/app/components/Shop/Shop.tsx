3acb3371f308d39fdec140860e37ad597c5e7828
Jewel-190237 <jewel190237@gmail.com>
2024-09-13 10:29:07 +0600

    shop updated

--- src/app/components/Shop/Shop.tsx
+++ src/app/components/Shop/Shop.tsx
@@ -22,19 +22,18 @@ const componentMap: Record<string, React.ReactNode> = {
 const Shop: React.FC = () => {
     const [activeTab, setActiveTab] = useState('all');
     const tabs = ['all', 'dumbbel', 'outfit', 'protine', 'bags', 'shoe', 'more'];
-
     return (
         <section className='max-w-[1320px] mx-auto my-10'>
             <BasicComponent subHeading='shop' heading='here you can buy any thing' />
             <div className="">
-                <div className="flex justify-center mb-4 border w-[51.3%] mx-auto rounded">
+                <div className="flex justify-center mb-4 border w-[100%] md:w-[66.5%] lg:w-[51.3%] mx-auto rounded">
                     {tabs.map(tab => (
                         <button
                             key={tab}
-                            className={`px-4 rounded py-4 ${activeTab === tab ? 'bg-[#E67529] text-white' : 'text-black bg-white'}`}
+                            className={`px-2 md:px-4 rounded py-2 md:py-4 ${activeTab === tab ? 'bg-[#E67529] text-white' : 'text-black bg-white'}`}
                             onClick={() => setActiveTab(tab)}
                         >
-                            <div className='px-3'>
+                            <div className=' lg:px-3'>
                                 {tab.charAt(0).toUpperCase() + tab.slice(1)}
                             </div>
                         </button>
