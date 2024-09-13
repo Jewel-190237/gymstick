23822f92f82416b51facc8d81f133f3def573eb8
Jewel-190237 <jewel190237@gmail.com>
2024-09-13 18:13:19 +0600

    file added

--- src/app/components/card/ShopCard.tsx
+++ src/app/components/card/ShopCard.tsx
@@ -13,7 +13,7 @@ interface ShopCardProps {
   }
 const ShopCard: React.FC<ShopCardProps> = ({status, rate, name, price, image}) => {
     return (
-        <div className="rounded">
+        <div className="rounded hover:scale-105 transition-all duration-150">
             <div className='relative status bg-primary w-[115px] h-[96px] flex items-center'> <p className='text-white pl-4 -rotate-45'>{status}</p> </div>
             <div className='bg-[#D9D9D9] -mt-24'>
                 <Image src={image} alt="shop" width={270} height={294} className="mx-auto" />
@@ -21,7 +21,7 @@ const ShopCard: React.FC<ShopCardProps> = ({status, rate, name, price, image}) =
             <div className="shadow-custom-light text-center flex flex-col gap-2 py-6">
                 <h6 className="capitalize text-[16px] font">{name}</h6>
                 <Rate defaultValue={rate} />
-                <h6 className="text-[16px] font-semibold">{price}</h6>
+                <h6 className="text-[16px] font-semibold">${price}</h6>
                 <div className="flex gap-[14px] justify-center">
                     <button className="p-1 hover:bg-primary hover:text-white text-primary bg-white border border-primary rounded cursor-pointer">
                         <TbShoppingBag size={24} />
