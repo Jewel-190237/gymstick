6aa4defd0271dc09afc19f7d21fcb8d47f2a6c4e
Jewel-190237 <190237@ku.ac.bd>
2024-09-16 11:50:13 +0600

    file added

--- src/app/components/shared/BasicHeader.tsx
+++ src/app/components/shared/BasicHeader.tsx
@@ -15,7 +15,7 @@ const BasicHeader: React.FC<BasicHeaderProps> = ({ heading, subHeading }) => {
         >
             <div className="absolute inset-0 bg-black bg-opacity-80"></div>
             <div className="relative font-montserrat text-white text-5xl max-w-[1320px] mx-auto pt-[219px]">
-                <h2 className="text-5xl font-bold">{heading}</h2>
+                <h2 className="font-bold">{heading}</h2>
                 <div className="mt-[14.5px] flex items-center gap-2 text-[18px] font-semibold">
                     <IoHome className="text-primary" />
                     <p className="text-primary">{subHeading}</p>
--- src/app/components/singlePage/Wheyprotein.tsx
+++ src/app/components/singlePage/Wheyprotein.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import BasicHeader from '../shared/BasicHeader';
 import Image from 'next/image';
 import { Rate } from 'antd';
-
+import { BsCartPlus } from "react-icons/bs";
+import { FaFacebookF, FaLinkedinIn, FaRegHeart } from "react-icons/fa";
+import { TbBrandYoutube } from "react-icons/tb";
+import { FaXTwitter } from 'react-icons/fa6';
+import All from '../Shop/All';
 const Wheyprotein: React.FC = () => {
+    const [activeTab, setActiveTab] = useState('description');
+    const tabs = ['description', 'review']
     return (
         <section>
             <BasicHeader heading='Whey Protein' subHeading='Home' />
@@ -27,12 +34,49 @@ const Wheyprotein: React.FC = () => {
                             <p className='text-[16px] font-normal capitalize'>In stock</p>
                         </div>
                         <p className='mt-[40px] text-[18px] font-normal'>Description</p>
+                        <p className='mt-6 text-secondary text-justify text-[16px] font-normal'>Impact Whey Protein is a high-quality, fast-absorbing protein powder that supp
+                            orts muscle growth and recovery. Packed with essential amino acids, it is perfect for post-workout nutrition or boosting daily protein intake. Ideal for athletes and fitness enthusiasts looking to build strength and improve performance. Impact Whey Protein is a high-quality, fast-absorbing protein powder that supports muscle growth and recovery. </p>
+                        <p className='mt-10 text-[18px] font-medium'>Size:</p>
+                        <div className='mt-4 flex items-center space-x-6'>
+                            <button className='product-button'>1.5 KG</button>
+                            <button className='product-button'>2.5 KG</button>
+                            <button className='product-button'>5 KG</button>
+                        </div>
+                        <div className='mt-10 flex items-center space-x-6'>
+                            <button className='product-button flex items-center space-x-[10px]'><span>Add to Cart</span> <BsCartPlus size={20} /> </button>
+                            <button className='product-button flex items-center space-x-[10px]'><span>Add to Cart</span> <FaRegHeart /> </button>
+                        </div>
+                        <p className='mt-6 text-secondary text-[16px] font-normal capitalize'>is it sold out? <span className='text-primary underline'>join the waiting list.</span></p>
+                        <div className='mt-12 flex items-center space-x-5'>
+                            <p className='text-[18px] font-medium'>Share:</p>
+                            <button className='share-button ml-1'> <TbBrandYoutube size={24} /> </button>
+                            <button className='share-button'> <FaFacebookF size={24} /> </button>
+                            <button className='share-button'> <FaXTwitter size={24} /> </button>
+                            <button className='share-button'> <FaLinkedinIn size={24} /> </button>
+                        </div>
                     </div>
                 </div>
-
+                <div className='mt-[140px]'>
+                    <div className="flex space-x-[35px] ">
+                        {tabs.map(tab => (
+                            <button
+                                key={tab}
+                                className={`px-7 rounded py-3 ${activeTab === tab ? 'bg-[#E67529] text-white' : 'text-black border border-[#D9D9D9] bg-white'}`}
+                                onClick={() => setActiveTab(tab)}
+                            >
+                                <div className=''>
+                                    {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                                </div>
+                            </button>
+                        ))}
+                    </div>
+                    <div className="p-4">
+                        {activeTab === 'description' && <p className='text-justify font-normal text-[16px] text-[#2B2B2BCC]'>Description It is a long established to fact that is one reader will be the distracted by the readable content of the page when this looking at its layout. The point of using Lorem Ipsum is that it is has normal distribution of letters, as opposed to using contents here. Lorem is an long established to fact that is one reader will be the distracted by the readable content.Description It is a long established to fact that is one reader will be the distracted by the readable content of the page when this looking at its layout. The point of using Lorem Ipsum is that it is has normal distribution of letters, as opposed to using contents here. Lorem is an long established to fact that is one reader will be the distracted by the readable content.</p> }
+                        {activeTab === 'review' && <All/>}
+                    </div>
+                </div>
             </div>
         </section>
     );
 };
-
 export default Wheyprotein;
